b0d407e969938ea3f5843ba427a83e7261fa8a83
unknown <frankdavid93om@yahoo.com>
2022-05-21 14:49:07 -0400

    add

---
+++ Assignment_2/src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int pantherID;
+    private int score;
+
+    /**
+     *  Get Methods
+     */
+    public String getFirstName(){ return firstName};
+    public String getFirstName(){ return firstName}
+    public String getFirstName(){ return firstName}
+    public String getFirstName(){ return firstName}
+
+
+    // Constructor
+    public Student(String firstName, String lastName, int pantherID, int score){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pantherID = pantherID;
+        this.score = score;
+    }
+
+}
