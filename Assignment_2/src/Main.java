c66a3e0e4b05335dafa04520b4b70ee07cb4af63
unknown <frankdavid93om@yahoo.com>
2022-05-21 14:48:41 -0400

    rm

--- Assignment_2/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main (String[] args){
-        GradeBook myGradebook = new GradeBook();
-        Student s = new Student("Ann", "X",87,1234566);
-    }
-}
