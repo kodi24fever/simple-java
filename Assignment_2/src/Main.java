ce749012388a716365eb1fb33bc21ee536672073
unknown <frankdavid93om@yahoo.com>
2022-05-21 14:37:23 -0400

    test

---
+++ Assignment_2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args){
+        GradeBook myGradebook = new GradeBook();
+        Student s = new Student("Ann", "X",87,1234566);
+    }
+}
