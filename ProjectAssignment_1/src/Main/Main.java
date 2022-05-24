a726177b8def7196449d75c46df1bafc94182f7b
unknown <frankdavid93om@yahoo.com>
2022-05-23 22:58:24 -0400

    add welcoming loop for user input

--- ProjectAssignment_1/src/Main/Main.java
+++ ProjectAssignment_1/src/Main/Main.java
@@ -1,21 +1,26 @@
 package Main;
 import util.*;
 
+import java.util.Locale;
 import java.util.Scanner; // Import java.util.Scanner for getting user input
 import java.util.ArrayList; // Import arrrayList to create studnets
 
 public class Main {
     public static void main(String[] args) {
-
         // ArrayList of students
         ArrayList<Student> studentsList = new ArrayList<Student>();
 
         // Array to split user input in long string format
         String splited[];
 
+        // Scanner for user input
         Scanner myObj = new Scanner(System.in); // Scanner for user input
 
-        if(studentsList.size() == 0){
+        // While loop when lists is empty
+        while(studentsList.size() < 1){
+            /**
+             *  Welcome message for user
+             */
             // This message should show when student list is empty
             System.out.println("Welcome to my grade book!");
             System.out.println("Please enter the information of the first student using the following format:");
@@ -23,8 +28,8 @@ public class Main {
             System.out.println("Press Enter when you are done");
 
             String wholeStudent = myObj.nextLine();
+            // Splits whole input string into separate strings in an array
             splited = wholeStudent.split(" ");
-            System.out.println("Username is: " + wholeStudent + splited[0] + splited[1] + splited[2]);
 
             //Getting Student OBJ
             Student student = new Student(splited[0], splited[1], Integer.parseInt(splited[2]), Integer.parseInt(splited[3]));
@@ -38,10 +43,17 @@ public class Main {
                 studentsList.add(student);
                 System.out.println(studentsList);
             }else System.out.println("Sorry Try again!");
+        }
 
 
 
-        } else System.out.println("You added the student Buy!");
+
+
+
+
+//        if(wholeStudent.toLowerCase().matches("done")){
+//        }
+
 
 
 
