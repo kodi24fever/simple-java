ecb5e73c351999de0a6a48bce6a71b69305ca058
unknown <frankdavid93om@yahoo.com>
2022-05-26 10:58:42 -0400

    add assignment 4 package

---
+++ Assignment_4/src/question1/First.java
@@ -0,0 +1,18 @@
+package question1;
+
+
+class First
+
+{
+
+    public First()
+
+    {
+
+        System.out.print("a");
+
+    }
+
+    public String toString(){ return "F";}
+
+}
---
+++ Assignment_4/src/question1/Second.java
@@ -0,0 +1,17 @@
+package question1;
+
+class Second extends First
+
+{
+
+    public Second()
+
+    {
+
+        System.out.print("b");
+
+    }
+
+    public String toString(){ return super.toString()+"S";}
+
+}
