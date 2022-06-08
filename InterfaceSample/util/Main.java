16849f9730305149a6e677444926f0a24332fce1
kodi24fever <frankdavid93om@yahoo.com>
2022-06-08 13:31:58 -0400

    add main to test example

---
+++ InterfaceSample/util/Main.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args){
+        ArrayList<Drivable> listOfDrivableObjects = new ArrayList<>();
+
+        Drivable temp = new Car("Nissan");
+        temp.accelerate(20);
+        listOfDrivableObjects.add(temp);
+        
+        for (Drivable drive: listOfDrivableObjects)
+             System.out.println(drive.toString() + " speeds to: " + drive.getSpeed() + " km/h");
+
+    }
+
+}
