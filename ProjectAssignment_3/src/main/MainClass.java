3f95308ff2f813d52e589ea3f31c74096f924484
unknown <frankdavid93om@yahoo.com>
2022-06-19 14:55:17 -0400

    change mainClass to recieve multiple inputs at the same time

--- ProjectAssignment_3/src/main/MainClass.java
+++ ProjectAssignment_3/src/main/MainClass.java
@@ -1,7 +1,6 @@
 package main;
 
 import java.io.*;
-import java.sql.SQLSyntaxErrorException;
 import java.util.*;
 import util.*;
 
@@ -26,7 +25,7 @@ public class MainClass {
         }
     }
 
-    public static void main(String[] args) throws IOException, NumberFormatException {
+    public static void main(String[] args) throws IOException, NumberFormatException, NullPointerException {
 
         // Scanner object
         Scanner userInput = new Scanner(System.in);
@@ -34,8 +33,8 @@ public class MainClass {
         //printer object
         PrintWriter out = new PrintWriter(OUTPUT + "out.csv");
 
-        //Array list to temporarily store ids to sort them for TA's and get maximum value
-        ArrayList<Integer> saveAllIds = new ArrayList<>();
+        // Validate object
+        ValidateInformation val = new ValidateInformation();
 
         String splitted[] = {};
         String splitFullName[] = {};
@@ -50,7 +49,7 @@ public class MainClass {
 
         System.out.println("Please enter the total amount of students, teachers, and TA's in a number format");
 
-        // Here I have to check if the value is an integer
+        // This while loop checks if the value is an integer
         while(true){
             String totalStudentsToEnter = userInput.nextLine();
             try{
@@ -64,20 +63,34 @@ public class MainClass {
             }
         }
 
-        while(totalLines > 0){
-            System.out.println("Enter Information in the format \"Position Firstname,Lastname StudentID TeacherID PhoneNumber\"");
+        //Array of CSVPrintable objects to be able to have multiple input lines
+        Integer[] inputLines = new Integer[totalLines];
+
+        // Welcoming message
+        System.out.println("Enter Information in the format \"Position Firstname,Lastname StudentID TeacherID PhoneNumber\"");
+        System.out.println("If the position entered is Teacher, StudentID must be 0. If position entered is Student, TeacherID must be 0.");
+
+        for(int i = 0; i < inputLines.length; i++){
+            int errorLine = i + 1;
 
             String inputInformation = userInput.nextLine();
             splitted = inputInformation.split(" ");
 
-            if(splitted.length != 5){
-                System.out.println("Information is not accurate. Try again!");
-            }else {
+            // Assigning position token
+            position = splitted[0];
 
+            if(splitted.length != 5){
+                System.out.println("Information is not accurate in line " + errorLine + ". The format is \"Position Firstname,Lastname StudentID TeacherID PhoneNumber\" Try again!");
+                i--;
+                continue;
+            }else {
                 // Here check if names contains comma if not throw error
+                // Getting the tokens. Tokens for firstname and lastname.
                 fullNameWithComma = splitted[1];
                 if(!fullNameWithComma.contains(",")){
-                    System.out.println("Name is not well formatted. Make sure is Firstname\",\"Lastname with the comma in between and no spaces.");
+                    System.out.println("Name is not well formatted at line " + errorLine + ". Make sure is Firstname\",\"Lastname with the comma in between and no spaces.");
+                    System.out.println("Example: \"John,Doe\"");
+                    i--;
                     continue;
                 }else {
                     /**
@@ -86,10 +99,12 @@ public class MainClass {
                      */
                     splitFullName = fullNameWithComma.split(",");
                     if(!splitFullName[0].matches("[a-zA-Z.]+")){
-                        System.out.println("Name is not well formatted. Make sure FirstName must be written with alphabetic letters not numbers.");
+                        System.out.println("Name is not well formatted at line " + errorLine + ". Make sure FirstName must be written with alphabetic letters not numbers.");
+                        i--;
                         continue;
                     }else if(!splitFullName[1].matches("[a-zA-Z.]+")){
-                        System.out.println("Name is not well formatted. Make sure LastName must be written with alphabetic letters not numbers.");
+                        System.out.println("Name is not well formatted at line " + errorLine + ". Make sure LastName must be written with alphabetic letters not numbers.");
+                        i--;
                         continue;
                     }else {
                         // Assigns firstname and lastname to variables if everything is correct
@@ -98,72 +113,110 @@ public class MainClass {
                     }
                 }
 
+                // Error handling for ID's and phone if user input other variables besides Integers
+                try{
+                    studentID = Integer.parseInt(splitted[2]);
+                }catch (NumberFormatException exception){
+                    System.out.println("Student ID must be an integer value of 5 digits at line " + errorLine);
+                    i--;
+                    continue;
+                }
+                try{
+                    teacherID = Integer.parseInt(splitted[3]);
+                }catch (NumberFormatException exception){
+                    System.out.println("Teacher ID must be an integer value of 5 digits at line " + errorLine);
+                    i--;
+                    continue;
+                }
+                try{
+                    phoneNUmber = Long.parseLong(splitted[4]);
+                }catch (NumberFormatException exception){
+                    System.out.println("Phone number must be written as integer values of 10 digits at line " + errorLine);
+                    i--;
+                    continue;
+                }
 
-                // Getting the tokens. Tokens for firstname and lastname were created before
-                position = splitted[0];
-                studentID = Integer.parseInt(splitted[2]);
-                teacherID = Integer.parseInt(splitted[3]);
-                phoneNUmber = Long.parseLong(splitted[4]);
-
-                // Validate object
-                ValidateInformation val = new ValidateInformation();
-
+                // Validates ID's and phone number for each position
                 if(val.isIDValid(studentID) && val.isIDValid(teacherID) && val.isPhoneNumberValid(phoneNUmber)){
 
                     // Starts command phase
                     if(position.toLowerCase().matches("teacher")){
 
-                        if(studentID != 0){
-
-                            System.out.println("When position is teacher student ID must be 0. Try again!");
-                        }else {
+                        if(String.valueOf(teacherID).length() != 5){
+                            System.out.println("TeacherID must be a number of 5 digits.");
+                            i--;
+                            continue;
+                        }else if(studentID != 0){
+                            System.out.println("Error at line " + errorLine + ".When position is teacher student ID must be 0. Try again!");
+                            i--;
+                            continue;
+                        }else{
+                            // Writes teacher to csv
                             String last4Digits = splitted[4].substring((splitted[4].length() - 4), splitted[4].length());
-                            Teacher teacher = new Teacher(firstName,lastName,teacherID,Integer.parseInt(last4Digits));
+                            new Teacher(firstName,lastName,teacherID,Integer.parseInt(last4Digits)).csvPrintln();
 
-                            // Add id to ArrayList
-                            saveAllIds.add(teacherID);
-                            //Print info to csv
-                            teacher.csvPrintln();
-                            // Total Lines decrease to close loop
-                            totalLines--;
+                            System.out.println("Teacher information successfully stored at line " + errorLine);
                         }
                     } else if(position.toLowerCase().matches("student")) {
 
-                        if(teacherID != 0){
-                            System.out.println("When position is student, teacher ID must be 0. Try again!");
+                        if(String.valueOf(studentID).length() != 5){
+                            System.out.println("StudentID must be a number of 5 digits.");
+                            i--;
+                            continue;
+                        }else if(teacherID != 0){
+                            System.out.println("Error at line " + errorLine + ". When position is student, teacher ID must be 0. Try again!");
+                            i--;
+                            continue;
                         }else {
-                            Student student = new Student(firstName, lastName, studentID, phoneNUmber);
-
-                            // Add id to ArrayList
-                            saveAllIds.add(studentID);
-
-                            //Prints info to csv
-                            student.csvPrintln();
-
-                            // Total Lines decrease to close loop
-                            totalLines--;
+                            // Writes student to csv
+                            new Student(firstName, lastName, studentID, phoneNUmber).csvPrintln();
+                            System.out.println("Student information successfully stored at line " + errorLine);
                         }
                     }else if(position.toLowerCase().matches("ta")) {
 
-                        // Collections.max adds to TA the max value of ID's in the arrayList
-                        TA ta = new TA(firstName, lastName, studentID, teacherID, phoneNUmber);
-
-                        //Prints info to csv
-                        ta.csvPrintln();
-
-                        // Total Lines decrease to close loop
-                        totalLines--;
-                    } else System.out.println("Wrong position. Must be \"teacher, student, or ta\". Try again!");
+                        if(String.valueOf(studentID).length() != 5) {
+                            System.out.println("StudentID must be a number of 5 digits.");
+                            i--;
+                            continue;
+                        }else if(String.valueOf(teacherID).length() != 5) {
+                            System.out.println("TeacherID must be a number of 5 digits.");
+                            i--;
+                            continue;
+                        }else {
+                            // Writes TA to csv
+                            new TA(firstName, lastName, studentID, teacherID, phoneNUmber).csvPrintln();
+                            System.out.println("TA information successfully stored at line " + errorLine);
+                        }
+                    } else {
+                        System.out.println("Wrong position at line " + errorLine + ". Must be \"teacher, student, or ta\", instead of " + "\"" +  position + "\"" +" Try again!");
+                        i--;
+                        continue;
+                    }
 
                 } else {
                     // General error handling if ids and phone numbers are wrong
-                    if(!val.isIDValid(studentID)) {System.out.println("Wrong ID. Student ID must be 5 digits or 0, depending of the position. Try again!");}
-                    if(!val.isIDValid(teacherID)) {System.out.println("Wrong ID. Teacher ID must be 5 digits or 0, depending of the position. Try again!");}
-                    if(!val.isPhoneNumberValid(phoneNUmber)) {System.out.println("Phone Number is not 10 digits. Try again!");}
+                    if(!val.isIDValid(studentID)) {
+                        if(position.toLowerCase().matches("teacher")){
+                            System.out.println("Wrong StudentID at line " + errorLine + ". Student ID must be 0, instead of \"" + studentID + "\".Try again!");
+                        }else System.out.println("Wrong StudentID at line " + errorLine + ". Student ID must be 5 digits, instead of \"" + studentID + "\".Try again!");
+                        i--;
+                        continue;
+                    }
+                    if(!val.isIDValid(teacherID)) {
+                        if(position.toLowerCase().matches("student")){
+                            System.out.println("Wrong TeacherID at line " + errorLine + ". Teacher ID must be 0, instead of \"" + teacherID + "\".Try again!");
+                        }else System.out.println("Wrong TeacherID at line " + errorLine + ". Teacher ID must be 5 digits, instead of \"" + teacherID + "\".Try again!");
+                        i--;
+                        continue;
+                    }
+                    if(!val.isPhoneNumberValid(phoneNUmber)) {
+                        System.out.println("Phone Number is not 10 digits at line " + errorLine + ". Try again!");
+                        i--;
+                        continue;
+                    }
                 }
 
             }
         }
-
     }
 }
