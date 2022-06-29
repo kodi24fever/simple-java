d786367d281c6904be8478114527df3060191ad1
kodi24fever <frankdavid93om@yahoo.com>
2022-06-29 17:45:05 -0400

    add try catch method for input values of strings length and maximum length

--- ProjectAssignment_4/main/PatternFinder.java
+++ ProjectAssignment_4/main/PatternFinder.java
@@ -27,25 +27,42 @@ public class PatternFinder {
 	Scanner keyboard = new Scanner(System.in);
 	//Step 1: handling input...
 	System.out.println("Enter the length of random string: ");
-	int patternMaxLength = 10;//you need to update this part so that the value is given by the user via keyboard!
+	//int patternMaxLength = 10;//you need to update this part so that the value is given by the user via keyboard!
 	int randomStringLength = keyboard.nextInt();
+
+	System.out.println("Enter the maximum length of special characters: ");
+	int patternMaxLength = keyboard.nextInt();
+
 	while (true) {
+
 	    try {
-		if (randomStringLength < 100000 || randomStringLength > 1000000000)
-		    throw new NumberFormatException();
+			if (randomStringLength < 100000 || randomStringLength > 1000000000)
+				throw new NumberFormatException();
 	    } catch (NumberFormatException e) {
-		System.out.println("Try again!");
-		randomStringLength = keyboard.nextInt();
-		continue;
+			System.out.println("Length of random string must be between 1 thousand and 1 billion. Try again!");
+			randomStringLength = keyboard.nextInt();
+			continue;
 	    }
+
+		try {
+			if(patternMaxLength < 5 || patternMaxLength > 15)
+				throw new NumberFormatException();
+		} catch (NumberFormatException e){
+			System.out.println("Pattern must be greater than 5 and less than 15. Try again!");
+			patternMaxLength = keyboard.nextInt();
+			continue;
+		}
+
 	    break;
 	}
+
+	
 	//Step 2: generating random string...
 	String randomString = randomStringGenerator(randomStringLength);
 	//Step 3: finding the interesting patterns
     	try {
     	    for (int length = patternMaxLength; length > 0; length--)
-    		singletonMiner(randomString, length);
+    			singletonMiner(randomString, length);
     	} catch (Exception exp) {
     	    System.out.println(exp.getMessage());
     	}
